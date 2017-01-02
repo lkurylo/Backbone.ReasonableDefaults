f0c5e2761fccd68b91538b6ba5bf7a19fc377c11
Łukasz Kuryło <secret@gmail.com>
2017-01-02 16:04:23 +0100

    added unit tests and libs for tests

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,8 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/underscore/underscore.js',
+      'node_modules/backbone/backbone.js',
       'src/*.js',
       'spec/*.js'
     ],
--- spec/Backbone.ReasonableDefaults.spec.js
+++ spec/Backbone.ReasonableDefaults.spec.js
@@ -1,10 +1,51 @@
-describe('test', function () {
-    it('test', function () {
-        console.log('test');
+describe('Backbone.ReasonableDefaults', function () {
+    it('exist property that is set only when plugin is loaded', function () {
+        var isLoaded = Backbone.Model.reasonableDefaultLoaded;
+
+        expect(isLoaded).toBe(true);
     });
 
-    it('test 2', function () {
-        console.log('test 2');
-        // throw new Exception('est');
-    })
+    it('can set custom fields when no `defaults` provided', function () {
+        var myModel = Backbone.Model.extend({});
+
+        var model = new myModel();
+
+        model.set({
+            'customField': true
+        });
+
+        expect(model.get('customField')).toBe(true);
+    });
+
+    it('can set fields that exists in `defaults`', function () {
+        var myModel = Backbone.Model.extend({
+            default: {
+                customField: false
+            }
+        });
+
+        var model = new myModel();
+
+        model.set({
+            'customField': true
+        });
+
+        expect(model.get('customField')).toBe(true);
+    });
+
+    it('cannot set fields that not exists in `defaults`', function () {
+        var myModel = Backbone.Model.extend({
+            default: {
+                customField: false
+            }
+        });
+
+        var model = new myModel();
+
+        expect(function () {
+            model.set({
+                'customField2': true
+            });
+        }).toThrow('Attr customField2 doesn\'t exists in the Backbone.Model defaults');
+    });
 });
